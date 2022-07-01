fe8ebc71efbc3656dbf40207238523299b88aced
Edwin3002 <vargas11742@gmail.com>
2022-06-30 23:32:00 -0500

    btn show

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -24,14 +24,14 @@ export const TaskList = () => {
 
   return (
     <div className='text-center'>
-      <h1 className='text-3xl font-bold text-white mt-4'>{fil? 'Incompleted': 'Completed'} Tasks {tasks ? tasks.filter(t=>(t.completed === fil)).length : 0}</h1>
+      <h1 className='text-3xl font-bold text-white mt-4'>{!fil ? 'Incompleted' : 'Completed'} Tasks {tasks ? tasks.filter(t => (t.completed === fil)).length : 0}</h1>
       <div className='flex flex-row-reverse m-2 mr-8'>
         <Link className=' justify-end bg-green-500 p-1 rounded-lg' to='create-task'>Create task</Link>
       </div>
       <div className='flex flex-wrap justify-around'>
         {
           tasks ?
-            tasks.filter(t=>(t.completed === fil)).map(task => (
+            tasks.filter(t => (t.completed === fil)).map(task => (
               <div key={task.id} className={task.completed ? 'bg-slate-800  flex flex-col rounded-xl w-80  p-2 m-2 relative' : 'bg-slate-500 flex flex-col rounded-xl w-80 p-2 m-2 relative'}>
                 <input className='absolute right-0 mr-6' type='checkbox' checked={task.completed} onChange={() => handleCheck(task.id)} />
                 <h3 className='text-2xl text-white'>
@@ -43,10 +43,10 @@ export const TaskList = () => {
                 <div className='flex justify-evenly'>
                   <button className='bg-red-500 p-1 rounded-lg hover:scale-110' onClick={() => handleDelete(task.id)} >Delete</button>
                   {
-                    !fil?
-                    <Link className='bg-yellow-500 p-1 rounded-lg hover:scale-110' to={`edit-task/${task.id}`}>Edit</Link>:
-                    ''
-                    
+                    !fil ?
+                      <Link className='bg-yellow-500 p-1 rounded-lg hover:scale-110' to={`edit-task/${task.id}`}>Edit</Link> :
+                      ''
+
                   }
                 </div>
               </div>
@@ -55,12 +55,7 @@ export const TaskList = () => {
         }
       </div>
       <div className='flex flex-row-reverse m-2 mr-8'>
-      {
-                    tasks.filter(t=>(t.completed === fil)).length !== 0?
-                    <span className='text-white hover:scale-110 justify-end bg-blue-600 p-1 rounded-lg' onClick={()=>setFil(!fil)}>Show  {fil? 'Incompleted': 'Completed'}</span>:
-                    ''
-                    
-                  }
+        <span className='text-white hover:scale-110 justify-end bg-blue-600 p-1 rounded-lg' onClick={() => setFil(!fil)}>Show  {fil ? 'Incompleted' : 'Completed'}</span>
       </div>
     </div >
   )
